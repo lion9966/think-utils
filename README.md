7b86efcb180bce83c6c616f9a08259b414f9eb90
lion9966 <lion9966@163.com>
2023-06-23 20:50:31 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
 # think-utils
+
 thinkphp 6 常用工具集合
+
+将thinkphp常用的：
+
+* 错误提示（think-exception），
+* 权限管理（think-permission），
+* JWT，
+* 集成腾讯短信验证，邮件验证码发送，
+* 密码
+* 随机数
+* 树
+
+### 安装
+
+```
+composer require lion9966/think-utils
+```
+
+### 使用方法
+
+```
+use lion9966\utils
+```
+
+* 错误提示（think-exception）：
+
+```php
+use lion9966\utils\exception\ResPonseCode;
+use lion9966\utils\exception\ResPonseJson;
+
+//通过server自动绑定think\exception\Handle，如果没绑定执行如下
+//需要在app/provide.php内定义：
+//'think\exception\Handle'=>\lion9966\utils\exception\ExceptionHandle::class
+//进行自定义错误处理
+
+//use lion9966\utils\exception\ExceptionHandle;
+```
+
+* 权限管理（think-permission）： 安装后，配置连接好数据库后，执行命令：
+
+```
+php think permission:install
+```
+
+进行数据库安装
+
+使用方法，具体见: lion9966\think-permission
+
+```php
+//引用
+use lion9966\utils\permission;
+//权限验证中间件
+use lion9966\utils\middleware;
+permission::class;
+```
+
+* JWT： 自动集成lcobucci/jwt 4.3.0，已设置好Token,中间件直接使用即可
+
+```php
+//jwt权限验证中间件
+use lion9966\utils\middleware;
+Auth::class;
+
+//引用Token方法
+use lion9966\utils\Token;
+```
+
+* 集成腾讯短信验证，邮件验证码发送： 自动集成腾讯qcloudsms/qcloudsms_php短信发送，可在此基础SmsCode改发送其他短信；
+  自动集成邮件swiftmailer/swiftmailer发送，可以在此基础上SendCodeMail改发送其他邮件； 默认只发送验证码
+
+* 密码： 密码产生和验证方法Password
+
+```php
+//引用方法
+use lion9966\utils\Password;
+```
+
+* 随机数： uuid和随机字符串产生方法Random
+
+```php
+//引用方法
+use lion9966\utils\Random;
+```
+
+* 树：
+
+```php
+//引用方法
+use lion9966\utils\Tree;
+```
+
+另外：验证/登录后用户数据
+
+```php
+//引用方法
+use lion9966\utils\data\UserData;
+//已在jwt验证后，绑定类
+bind('user-data', UserData::class);
+//信息初始化
+app('user-data')->withId($userId)->withData($user);
+```
+
+配置，具体见：
+
+```php
+//权限验证过滤
+auth;
+//权限管理数据库配置
+permission
+//工具集合综合配置（涉及到敏感信息，必须修改）
+utils
--- config/utils.php
+++ config/utils.php
@@ -3,32 +3,32 @@
 return [
     //jwt配置
     'jwt'      => [
-        'id '      => 'tcW9qG7JZOPDMpwE', //token的唯一标识
-        'issuer'   => 'zD08B2h4jcvyMuCR@ijoduo.com@eybiCdpXAWBN1Gah', //签发人
+        'id '      => 'ffsgsgwcxesxx', //token的唯一标识
+        'issuer'   => 'fsdswrwxxxhrtxz@ijoduo.com@rtddxxxxxcxxx', //签发人
         'audience' => '',//接收人 魔术方法 __construct 动态设置
-        'sign'     => 'LjqwbGHORS2XCWlQa5T4P1z0VxK3tu9D@ijoduo.com@Zoc521ELb6C0FuKe3BxljtRMfQnzgJU9', //签名密钥
+        'sign'     => 'fdbefsxdzsvzsfgasxxd@ijoduo.com@sdfs56fsfsscczxexcddggf', //签名密钥
         'expire'   => 3600 * 24, //有效期 8640 一天
     ],
     //腾讯云短信配置
     'codeSms'  => [
-        'app_id'        => '1400422851',
-        'app_key'       => '55faf5dcba04ef3a7502281e711b4216',
-        'sms_sign'      => '九朵科技',
-        'template_id'   => '712025',
+        'app_id'        => '1012345678',
+        'app_key'       => 'afsdf55fsdsgdsdxcdfdsgsdgsgs',
+        'sms_sign'      => 'xxxx科技',
+        'template_id'   => '612345',
         'expire'        => 900, //验证码过期时间
         'lenght'        => 4,   //验证码长度
         'register_open' => 1,   //注册开放
     ],
     //密码
     'password' => [
-        'salt' => 'ozbn3L2VWlOae8D5Tr9QHuIKiRXEBgpw',//盐
+        'salt' => 'fwrwwgver5cvdgdvfg5hdgf2xaw',//盐
     ],
     //可以配置在 mail.php 或 config.php 文件中, 但要保证能通过 mail.host 访问到配置信息
     'codeMail' => [
         'scheme'   => 'smtp', // 邮件驱动, 支持 smtp|sendmail|mail 三种驱动
         'host'     => 'smtp.163.com', // SMTP服务器地址
-        'username' => 'lion9966@163.com', // 发件邮箱名称
-        'password' => 'rose0917', // 发件邮箱密码
+        'username' => 'xxx@163.com', // 发件邮箱名称
+        'password' => 'xxxxxxxx', // 发件邮箱密码
         'port'     => 994, // SMTP服务器端口号,一般为25/465
 
         'options' => [],
@@ -38,8 +38,8 @@ return [
         'embed' => 'embed:', // 邮件中嵌入图片元数据标记
 
         'from' => [
-            'from'     => 'lion9966@163.com',
-            'fromName' => '九朵科技',
+            'from'     => 'xxx@163.com',
+            'fromName' => 'xxxx科技',
             'expire'   => 900, //验证码过期时间
             'lenght'   => 4,   //验证码长度
         ],
